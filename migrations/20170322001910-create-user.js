757bf090253729879f2cf93c04b083abb43ddac5
gaby <gmorenogaby@gmail.com>
2017-03-21 19:02:14 -0600

    Create Model and Migration for User

---
+++ migrations/20170322001910-create-user.js
@@ -0,0 +1,44 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      role: {
+        type: Sequelize.STRING
+      },
+      rank: {
+        type: Sequelize.INTEGER
+      },
+      info: {
+        type: Sequelize.STRING
+      },
+      page: {
+        type: Sequelize.TEXT
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Users');
+  }
+};
---
+++ models/user.js
@@ -0,0 +1,25 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define('user', {
+    id: (
+    	allowNull: false,
+    	defaultValue: DataTypes.UUIDV4
+    	primaryKey: true,
+    	type: DataTypes.UUID,
+    ),
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    role: DataTypes.STRING,
+    rank: DataTypes.INTEGER,
+    info: DataTypes.STRING,
+    page: DataTypes.TEXT,
+    image: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return User;
+};
